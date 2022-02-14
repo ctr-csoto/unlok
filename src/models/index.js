db4d0b38eb3ad42ef0febd173fdd17433ec8255d
Andres Manzano <amanzano@lean-tech.io>
2022-02-14 07:13:29 -0500

    ..

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,1 @@
-const generateRiteWayModels = require('ritewayautotransport_database');
-const RitewayDB = require('../config/database');
 
-const ritewayDB = new RitewayDB();
-
-module.exports = generateRiteWayModels(ritewayDB.connect());
